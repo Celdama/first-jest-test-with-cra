6814c0431b8dd4d04a1ab9d6e62c31aba2ca43e8
Celdama <celdama.dev@gmail.com>
2022-04-13 08:00:20 +0200

    App.: first test

--- src/App.test.js
+++ src/App.test.js
@@ -5,16 +5,23 @@ import '@testing-library/jest-dom';
 // provides the userEvent API that simulates user interactions with the webpage.
 // Alternatively, we could import the fireEvent API from @testing-library/react
 // fireEvent is an inferior counterpart to userEvent, so userEvent should always be preferred in practice
-import userEvent from '@testing-library/user-event';
-import TestComponent from 'path-to-test-component';
+// import userEvent from '@testing-library/user-event';
+// import TestComponent from 'path-to-test-component';
 import App from './App';
 // no need to import Jest, it will automatically detect jest files
 
 // all this above package come preinstalled and the scripts preconfigured with cra
 
-test('renders learn react link', () => {
-  // https://testing-library.com/docs/react-testing-library/api/#render
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   // https://testing-library.com/docs/react-testing-library/api/#render
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('App component', () => {
+  it('renders correct heading', () => {
+    const { getByRole } = render(<App />);
+    expect(getByRole('heading').textContent).toMatch(/our first test/i);
+  });
 });
