ec061a65ff08b40447cc0f0a525b3f60193a166d
Celdama <celdama.dev@gmail.com>
2022-04-13 07:57:19 +0200

    App.test: setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,19 @@
+// give us access to useful functions like render
 import { render, screen } from '@testing-library/react';
+// includes some handy custom matchers (assertive functions) like toBeInTheDocument and more.
+import '@testing-library/jest-dom';
+// provides the userEvent API that simulates user interactions with the webpage.
+// Alternatively, we could import the fireEvent API from @testing-library/react
+// fireEvent is an inferior counterpart to userEvent, so userEvent should always be preferred in practice
+import userEvent from '@testing-library/user-event';
+import TestComponent from 'path-to-test-component';
 import App from './App';
+// no need to import Jest, it will automatically detect jest files
+
+// all this above package come preinstalled and the scripts preconfigured with cra
 
 test('renders learn react link', () => {
+  // https://testing-library.com/docs/react-testing-library/api/#render
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
